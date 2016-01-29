bb91b440a6dff40c9dad4ebbfca7072db4c99bf0
Ali Raza <ali@sweetpixelstudios.com>
2016-01-29 11:45:14 +0500

    offer layout needs to be set

--- app/assets/javascripts/offers/composeOfferCtrl.js
+++ app/assets/javascripts/offers/composeOfferCtrl.js
@@ -2,11 +2,11 @@ angular.module('multifiApp')
 .controller('composeOfferCtrl',[
   '$scope','$http','$state',
   function ($scope, $http, $state) {
-    $scope.offer = {}
-    $scope.offer.title = 'Test Offer';
-    $scope.offer.details = 'Test Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab. Sed ut perspiciatis unde omnis iste natus error sit';
-    $scope.offer.termsconditions = 'Test Test Test';
-    $scope.offer.url = "app.multifi.io";
+    $scope.offer = {};
+    // $scope.offer.title = 'Test Offer';
+    // $scope.offer.details = 'Test Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab. Sed ut perspiciatis unde omnis iste natus error sit';
+    // $scope.offer.termsconditions = 'Test Test Test';
+    // $scope.offer.url = "app.multifi.io";
 
 
     $scope.createOffer = function () {
