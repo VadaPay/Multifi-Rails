55b36975163ec38848dce665bec5e7f64b2770a7
Ali Raza <ali@sweetpixelstudios.com>
2016-03-20 19:06:45 +0500

    offer page set

--- app/assets/javascripts/offers/viewOfferCtrl.js
+++ app/assets/javascripts/offers/viewOfferCtrl.js
@@ -1,14 +1,22 @@
 angular.module('multifiApp')
   .controller('viewOfferCtrl',
-  function ($scope, $stateParams, $http) {
+  function ($scope, $stateParams, $http, moment) {
 
     // perform the following:
     // 1. if the offer is expired show a message that it is expired
     // 2. If coupons are finsihed then show a message that this is expired
     // 3. if user pressed get coupon, show a form
 
+
     $http.get('/offers/' + $stateParams.id + '.json').then(
       function(response) {
         $scope.offer = response.data;
+        console.log($scope.offer);
+
+        $scope.interval = moment($scope.offer.expiry)
+        console.log($scope.interval);
+
+
+
       });
   });
