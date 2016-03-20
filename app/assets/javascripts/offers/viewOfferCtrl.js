0262418b200f890799d055e2fdb3346da24111eb
Ali Raza <ali@sweetpixelstudios.com>
2016-03-20 21:44:10 +0500

    customer addition modal

--- app/assets/javascripts/offers/viewOfferCtrl.js
+++ app/assets/javascripts/offers/viewOfferCtrl.js
@@ -1,6 +1,6 @@
 angular.module('multifiApp')
   .controller('viewOfferCtrl',
-  function ($scope, $stateParams, $http, moment) {
+  function ($scope, $stateParams, $http, moment, $uibModal) {
 
     // perform the following:
     // 1. if the offer is expired show a message that it is expired
@@ -15,8 +15,9 @@ angular.module('multifiApp')
 
         $scope.interval = moment($scope.offer.expiry)
         console.log($scope.interval);
-
-
-
       });
+
+      $scope.redeemOffer = function () {
+
+      };
   });
